8ba0fa48ce1bbf3cbdcb1545bc16287b34602fae
silva0g <silvanazinha@gmail.com>
2016-09-09 23:23:44 +0200

    added the bootstrap style to our application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909100321) do
+ActiveRecord::Schema.define(version: 20160909121116) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "tittle"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20160909100321) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
